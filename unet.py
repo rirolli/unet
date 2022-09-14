ba698eaf020e060c712f554fbba20b8b6b5c5b02
rirolli <riccardo.ung@outlook.it>
2022-09-14 18:15:29 +0200

    Finito il blocco per il decoder.

--- unet.py
+++ unet.py
@@ -1,6 +1,34 @@
 import string
 import torch
 import torch.nn as nn
+import torchvision.transforms as T
+
+class DoubleConvolutionalLayer(nn.Module):
+    """This class create a double convolutional layer with the following sequential structure:
+    - Convolutional Layer 1;
+    - ReLu Layer 1;
+    - Convolutional Layer 2;
+    - ReLu Layer 2;
+    """
+
+    def __init__(self, input_channels:int, output_channels:int, middle_channels:int=None, kernel_size=3, padding=1, bias=False) -> None:
+        super().__init__()
+        if middle_channels is None:
+            middle_channels = output_channels
+
+        self.conv1 = nn.Conv2d(in_channels=self.input_channels, out_channels=middle_channels, kernel_size=kernel_size, padding=padding, bias=bias)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=middle_channels, out_channels=self.output_channels, kernel_size=kernel_size, padding=padding, bias=bias)
+        self.relu2 = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        return x
+
+
 
 class EncoderBlock(nn.Module):
     """
@@ -13,57 +41,86 @@ class EncoderBlock(nn.Module):
         Parameters:
         - input_channels: input channels
         - output_channels: number of classes
+        - middle_channels: number of classes for intermediate passages
         - b_dropout: add the dropout layer (default = True)
         - p: probability of dropout
     """
 
-    def __init__(self, input_channels:int, output_channels:int, b_dropout=True, p=0.2):
+    def __init__(self, input_channels:int, output_channels:int, middle_channels:int=None, b_dropout=True, p=0.2) -> None:
         super().__init__()
-        self.input_channels = input_channels
-        self.output_channels = output_channels
         self.b_dropout = b_dropout
+        if middle_channels is None:
+            middle_channels = output_channels
 
         # layer of the block
-        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=output_channels, kernel_size=3, padding=1, bias=False)
-        self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(in_channels=output_channels, out_channels=output_channels, kernel_size=3, padding=1, bias=False)
-        self.relu2 = nn.ReLU()
+        self.d_conv = DoubleConvolutionalLayer(input_channels=input_channels, middle_channels=middle_channels, output_channels=output_channels)
         self.mp = nn.MaxPool2d(kernel_size=2)
         if self.b_dropout:
-            self.drop = nn.Dropout2d
+            self.drop = nn.Dropout2d(p=p)
 
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
+        x = self.d_conv(x)
         x = self.mp(x)
         if self.b_dropout:
             x = self.drop(x)
-        
         return x
 
 class DecoderBlock(nn.Module):
 
-    def __init__(self, input_channels:int, output_channels:int, b_dropout=True, p=0.2, mode="upsample"):
+    def __init__(self, input_channels:int, output_channels:int, middle_channels:int=None, b_dropout=True, p=0.2, mode="upsample"):
         """
-        mode: can be "upsample" or "convtranspose"
+        This class r4etur a Decoder Block for the UNet.
+
+        - mode: can be "upsample" or "convtranspose"
         """
         super().__init__()
-        self.input_channels = input_channels
-        self.output_channels = output_channels
         self.b_dropout = b_dropout
         self.mode = mode
 
+        if middle_channels is None:
+            middle_channels = output_channels
+
+        halved_channels = input_channels // 2  # In the decoder process the number of channels must be halved after the upsampling
+
         if mode=="upsample":
-            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.upscale = nn.Sequential(
+                nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+                nn.Conv2d(in_channels=input_channels, out_channels=halved_channels, kernel_size=2, bias=False)
+            )
         elif mode=="convtranspose":
-            nn.ConvTranspose2d(in_channels=input_channels, out_channels=input_channels*2, kernel_size=2, bias=False)
+            self.upscale = nn.ConvTranspose2d(in_channels=input_channels, out_channels=halved_channels, kernel_size=2, bias=False)
+
+        # the input_channels number have to be the same of the start because in this second step the halved output is concatenated
+        # with the corresponding encoder output layer.
+        self.d_conv = DoubleConvolutionalLayer(input_channels=input_channels, middle_channels=middle_channels, output_channels=output_channels)
+
+        if self.b_dropout:
+            self.drop = nn.Dropout2d(p=p)
+
+    def forward(self, x_encoder, x_decoder):
+        # first we have to upsampling the input and halves the channels
+        x_decoder_halved = self.upscale(x_decoder)
+
+        # crops the encoder output
+        h_decoder = x_decoder_halved.size()[2]
+        w_decoder = x_decoder_halved.size()[3]
+        x_encoder_cropped = T.CenterCrop(size=(h_decoder, w_decoder))(x_encoder)
+
+        # concatenate the halved decoder input with the cropped encoder output
+        x = torch.cat([x_encoder_cropped, x_decoder_halved], dim=1)
+
+        # go on with the double convolutional
+        x = self.d_conv(x)
+
+        # dropout layer
+        if self.b_dropout:
+            x = self.drop(x)
+
+        return x
 
 
-    def forward(self, x):
-        pass
+        
 
 class BlockFactory():
     """
